4fa6f2e03c116a4e8cbe5e5ec2f75a38cc1a5492
cardosofede <federico.cardoso.e@gmail.com>
2025-07-29 23:57:54 +0300

    (feat) move logging

--- mcp_hummingbot/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Utility modules for Hummingbot MCP Server"""
-
-from .logging import setup_logging
-from .validation import validate_trading_pair, validate_amount, parse_amount
-
-__all__ = ["setup_logging", "validate_trading_pair", "validate_amount", "parse_amount"]
