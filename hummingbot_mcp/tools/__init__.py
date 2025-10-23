20d2559e0f35561c7269eadfbb218a1262ff6998
cardosofede <federico.cardoso.e@gmail.com>
2025-10-23 16:53:58 -0300

    (feat) add gateway tools

--- hummingbot_mcp/tools/__init__.py
+++ hummingbot_mcp/tools/__init__.py
@@ -1,10 +1,21 @@
 """Tools module for Hummingbot MCP Server"""
 
 from .account import setup_connector
+from .gateway import manage_gateway_container, manage_gateway_config
+from .gateway_trading import manage_gateway_swaps
 
 # Tool registry for the MCP server
 TOOLS = [
     setup_connector,
+    manage_gateway_container,
+    manage_gateway_config,
+    manage_gateway_swaps,
 ]
 
-__all__ = ["TOOLS", "setup_connector"]
+__all__ = [
+    "TOOLS",
+    "setup_connector",
+    "manage_gateway_container",
+    "manage_gateway_config",
+    "manage_gateway_swaps",
+]
